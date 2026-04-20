72234317e0d22c6f160e232a89ba8a2ea68dead6
Ezekiel <oyedijo.ezekiel@piit.com.ng>
2026-04-20 12:17:09 +0100

    add studentService

--- src/main/java/io/javaapp/n1package/service/StudentService.java
+++ src/main/java/io/javaapp/n1package/service/StudentService.java
@@ -8,6 +8,12 @@ public class StudentService {
     StudentRepository studentRepository = new StudentRepository();
 
     public Student registerStudent(StudentDto studentDto) {
+        if(studentDto.getAge() < 18) {
+
+        }
+        else if(studentDto.getName().isEmpty()) {
+
+        }
         Student student = new Student(studentDto);
         studentRepository.save(student);
         return student;
